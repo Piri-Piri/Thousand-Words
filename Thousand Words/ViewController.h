022f81b8690a3a1cf5d0c0e8a21b188b913944e0
Piri-Piri <david.pirih@gmx.de>
2014-09-08 23:39:30 +0200

    Initial Commit

---
+++ Thousand Words/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thousand Words
+//
+//  Created by David Pirih on 08.09.14.
+//  Copyright (c) 2014 Piri-Piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Thousand Words/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by David Pirih on 08.09.14.
+//  Copyright (c) 2014 Piri-Piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
